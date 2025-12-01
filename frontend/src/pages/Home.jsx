4447783cf489c5dc67b460c7a8f484c8a5882a8b
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2025-12-01 20:51:30 +0530

    fixed another bug

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import Sports from '../components/Sports';
 import Footer from '../components/Footer';
-import FacilityCarousel from '../components/facilityCarousel';
+import FacilityCarousel from '../components/FacilityCarousel.jsx';
 function Home() {
   return (
     <>
