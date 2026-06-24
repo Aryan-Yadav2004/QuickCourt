5d59ce7b089e4c91ba127f66ddbbabbfe5d8c23e
Aryan Yadav <yadav122004aryan@example.com>
2026-06-25 00:01:08 +0530

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 
 app.use(cors({ 
-    origin: "http://localhost:5173",
+    origin: "http://16.171.18.60",
     credentials: true,
 }));
 //https://quickcourt-online.onrender.com
